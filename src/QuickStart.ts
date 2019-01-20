ff85456bacefc2a0c3bb3f0bb7d7e13d0b972379
Edison Lim <edison.limjh@gmail.com>
2019-01-20 13:34:53 +0800

    fix(): Use @zilliqa-js 0.4.0^.

--- src/QuickStart.ts
+++ src/QuickStart.ts
@@ -1,36 +1,43 @@
-import { BN, Long } from '@zilliqa-js/util';
-import { Zilliqa } from '@zilliqa-js/zilliqa';
-import * as CP from '@zilliqa-js/crypto';
+import { BN, Long, units, bytes } from "@zilliqa-js/util";
+import { Zilliqa } from "@zilliqa-js/zilliqa";
+import * as CP from "@zilliqa-js/crypto";
 
-const zilliqa = new Zilliqa('http://localhost:4200');
+// API endpoint
+const zilliqa = new Zilliqa("http://localhost:4200");
 
 //Populate the wallet with an account
-const privkey = 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3';
+const privkey = "db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3";
 
-zilliqa.wallet.addByPrivateKey(
-  privkey
-);
+// These are set by the core protocol, and may vary per-chain.
+// These numbers are JUST AN EXAMPLE. They will NOT WORK on the public testnet
+// or mainnet. Please check what they are before proceeding, or your
+// transactions will simply be rejected.
+const CHAIN_ID = 2;
+const MSG_VERSION = 1;
+const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
+
+zilliqa.wallet.addByPrivateKey(privkey);
 
 const add = CP.getAddressFromPrivateKey(privkey);
-console.log('Your account address is:');
+console.log("Your account address is:");
 console.log(`0x${add}`);
 
 async function testBlockchain() {
   try {
     //GetBalance
     const balance = await zilliqa.blockchain.getBalance(add);
-    console.log('Your account balance is:');
+    console.log("Your account balance is:");
     console.log(balance.result);
 
     //Send a transaction to the network
     const tx = await zilliqa.blockchain.createTransaction(
       zilliqa.transactions.new({
-        version: 1,
-        toAddr: '573EC96638C8BB1C386394602E1460634F02ADDA',
-        amount: new BN(888888),
-        gasPrice: new BN(1000000000),
-        gasLimit: Long.fromNumber(1),
-      }),
+        version: VERSION,
+        toAddr: "573EC96638C8BB1C386394602E1460634F02ADDA",
+        amount: new BN(units.toQa("888", units.Units.Zil)), // Sending an amount in Zil (888) and converting the amount to Qa
+        gasPrice: new BN(units.toQa("1000", units.Units.Li)), // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
+        gasLimit: Long.fromNumber(1)
+      })
     );
     console.log("The transaction status is:");
     // @ts-ignore
@@ -90,18 +97,18 @@ async function testBlockchain() {
     const init = [
       // this parameter is mandatory for all init arrays
       {
-        vname : '_scilla_version',
-        type : 'Uint32',
-        value : '0',
+        vname: "_scilla_version",
+        type: "Uint32",
+        value: "0"
       },
       {
-        vname: 'owner',
-        type: 'ByStr20',
+        vname: "owner",
+        type: "ByStr20",
         // NOTE: all byte strings passed to Scilla contracts _must_ be
         // prefixed with 0x. Failure to do so will result in the network
         // rejecting the transaction while consuming gas!
-        value: `0x${add}`,
-      },
+        value: `0x${add}`
+      }
     ];
 
     // Instance of class Contract
@@ -109,37 +116,40 @@ async function testBlockchain() {
 
     // Deploy the contract
     const [deployTx, hello] = await contract.deploy({
-      version: 1,
-      gasPrice: new BN(1000000000),
-      gasLimit: Long.fromNumber(2500),
+      version: VERSION,
+      gasPrice: units.toQa("1000", units.Units.Li),
+      gasLimit: Long.fromNumber(10000)
     });
 
     // Introspect the state of the underlying transaction
-    console.log('Deployment Transaction ID: ', deployTx.id);
-    console.log('Deployment Transaction Receipt: ', deployTx.txParams.receipt);
+    console.log("Deployment Transaction ID: ", deployTx.id);
+    console.log("Deployment Transaction Receipt: ", deployTx.txParams.receipt);
 
     // Get the deployed contract address
-    console.log('The contract address is:');
+    console.log("The contract address is:");
     console.log(hello.address);
-    const callTx = await hello.call('setHello', [
-      {
-        vname: 'msg',
-        type: 'String',
-        value: 'Hello World'
-      }],
+    const callTx = await hello.call(
+      "setHello",
+      [
+        {
+          vname: "msg",
+          type: "String",
+          value: "Hello World"
+        }
+      ],
       {
         // amount, gasPrice and gasLimit must be explicitly provided
-        version: 1,
+        version: VERSION,
         amount: new BN(0),
-        gasPrice: new BN(1000000000),
-        gasLimit: Long.fromNumber(2500),
+        gasPrice: units.toQa('1000', units.Units.Li),
+        gasLimit: Long.fromNumber(8000),
       }
     );
     console.log(callTx);
 
     //Get the contract state
     const state = await hello.getState();
-    console.log('The state of the contract is:');
+    console.log("The state of the contract is:");
     console.log(state);
   } catch (err) {
     console.log(err);
